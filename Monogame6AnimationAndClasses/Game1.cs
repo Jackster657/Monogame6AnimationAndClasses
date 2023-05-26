70daf6a95b71a3eb60cd80a624a5ce33d95df890
Jack Eitel <339481210@GoTVDSB.ca>
2023-05-26 10:02:02 -0400

    Add project files.

---
+++ Monogame6AnimationAndClasses/Game1.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Runtime.ExceptionServices;
+
+namespace Monogame6AnimationAndClasses
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        Texture2D tribbleBrownTexture;
+
+        Texture2D tribbleCreamTexture;
+
+
+        List<Texture2D> tribbleTextures;
+        List<TribbleClass> tribbles;
+
+        Texture2D tribbleTexture;
+        Rectangle tribbleGreyRect;
+        Texture tribbleGreyTexture;
+        Texture2D tribbleOrangeTexture;
+        SoundEffect hit;
+        int colour;
+
+        Vector2 tribbleGreySpeed;
+
+   
+        Random generator;
+
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            generator = new Random();
+            tribbleTextures = new List<Texture2D>();
+            tribbles = new List<TribbleClass>();
+            base.Initialize();
+
+            // Create random tribbles
+            for (int i = 0; i < 40; i++)
+                tribbles.Add(new TribbleClass(tribbleTextures[generator.Next(tribbleTextures.Count)], new Rectangle(300, 10, generator.Next(25, 300), generator.Next(25, 300)), new Vector2(generator.Next(1,4), generator.Next(1, 4)),  hit));
+            
+            
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            // TODO: use this.Content to load your game content here
+            
+            tribbleTextures.Add(Content.Load<Texture2D>("tribbleGrey"));
+            tribbleTextures.Add(Content.Load<Texture2D>("tribbleCream"));
+            tribbleTextures.Add(Content.Load<Texture2D>("tribbleBrown"));
+            tribbleTextures.Add(Content.Load<Texture2D>("tribbleOrange"));
+            hit = Content.Load<SoundEffect>("mwop");
+
+
+
+
+
+
+
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+            foreach (TribbleClass tribble in tribbles)
+                tribble.Move(_graphics);
+            
+
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            foreach (TribbleClass tribble in tribbles)
+                tribble.Draw(_spriteBatch);
+            _spriteBatch.End();
+
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Monogame6AnimationAndClasses/TribbleClass.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monogame6AnimationAndClasses
+{
+    internal class TribbleClass
+    {
+        private Texture2D _texture;
+        private Rectangle _rectangle;
+        private Vector2 _speed;
+        SoundEffect _coo;
+        public TribbleClass(Texture2D texture, Rectangle rect, Vector2 speed, SoundEffect coo)
+        {
+            _texture = texture;
+            _rectangle = rect;
+            _speed = speed;
+            _coo = coo;
+        }
+        public Texture2D Texture
+        {
+            get { return _texture; }
+        }
+        public Rectangle Bounds
+        {
+            get { return _rectangle; }
+            set { _rectangle = value; }
+        }
+        public void PlayCoo()
+        {
+            _coo.Play();
+        }
+        public void Move(GraphicsDeviceManager _graphics)
+        {
+            _rectangle.Offset(_speed);
+            if (_rectangle.Left < 0 || _rectangle.Right > _graphics.PreferredBackBufferWidth)
+            {
+                _speed.X *= -1;
+                
+                PlayCoo();
+            }
+            if (_rectangle.Top < 0 || _rectangle.Bottom > _graphics.PreferredBackBufferHeight)
+            {
+                _speed.Y *= -1;
+
+                PlayCoo();
+            }
+
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(_texture, _rectangle, Color.White);
+        }
+    }
+}
